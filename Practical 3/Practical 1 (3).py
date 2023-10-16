3868a66e212f60c6d91a83c922e2ebf7581360e9
Joel <147464047+JOEL-7125@users.noreply.github.com>
2023-10-16 21:35:44 +0530

    Update Practical 1 (3).py

--- Practical 3/Practical 1 (3).py
+++ Practical 3/Practical 1 (3).py
@@ -1,3 +1,6 @@
+# Q : Write a Python program to demonstrate different types of function argument
+# a) Keyword argument
+
 def information(name, age):
     print("Name:", name);
     print("Age:", age);
