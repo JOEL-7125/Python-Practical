ee7fa626a5ff680f5b0f0b9448d4ba37fd2911c8
Joel <147464047+JOEL-7125@users.noreply.github.com>
2023-10-16 21:36:29 +0530

    Update Practical 3 (3).py

--- Practical 3/Practical 3 (3).py
+++ Practical 3/Practical 3 (3).py
@@ -1,3 +1,6 @@
+# Q : Write a Python program to demonstrate different types of function argument
+# a) Arbitrary argument
+
 def greet(*names):
     for name in names:
         print("Good morning", name);
