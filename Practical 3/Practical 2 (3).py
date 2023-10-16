5e1d311bf3448ea82746d2042636604dccf6ed0b
Joel <147464047+JOEL-7125@users.noreply.github.com>
2023-10-16 21:36:02 +0530

    Update Practical 2 (3).py

--- Practical 3/Practical 2 (3).py
+++ Practical 3/Practical 2 (3).py
@@ -1,3 +1,6 @@
+# Q : Write a Python program to demonstrate different types of function argument
+# b) Default argument
+
 def information(name, age=55):
     print("Name:", name);
     print("Age:", age);
